ac1ff35048ee690df91bc379aa067398b71bbd65
bgajdero <bgajdero@gmail.com>
2013-01-07 19:45:43 -0500

    created rails app

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 social-movies
 =============
-`git clone git clone https://github.com/bgajdero/social-movies.git social-movies`
+`git clone https://github.com/bgajdero/social-movies.git social-movies`
+
 `cd social-movies`
+
 `bundle install`
 
+`bundle exec rails server`
 
 Rotten tomato API: e89w8cn3hx4774mqj7esu5ep
 
+Disqus API KEY: gWkAYp5jgUNlJlXBQC1uL0n2LKorKcZMjk1U7NEk4oIyHUX0vQod7wDJsp1Cy8yl
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMovies::Application.config.secret_token = '382de09c8930bf1ee5e16a2e3e895e40b19ce829bcfda9095f234deb2875e91ee0b51e787b22dea57fd8adc35def46d100ce17244be54f0eaf2e1fb85c95286d'
